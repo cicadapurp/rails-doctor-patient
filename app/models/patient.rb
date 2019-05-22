84772c1b2eda85e23211ca48128a9838d8d78132
tallan groberg <tallan.taven@gmail.com>
2019-05-21 22:36:36 -0600

    bug fix start

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,9 @@
 class Patient < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :doctors, through: :appointmemts
+
+  def full_name
+    "#{self.first_name} #{last_name}"
+
+  end
 end
